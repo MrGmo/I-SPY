f8080ab72be1e37578a244e9dc6151249727bf56
Guillermo <gaguilar128@gmail.com>
2021-12-20 11:02:37 -0600

    Fixed URLS

--- frontend/src/api/UserAPI.js
+++ frontend/src/api/UserAPI.js
@@ -2,7 +2,7 @@ const BASE_URL = process.env.REACT_APP_USER_API_URL
 
 
 const login = (userObject) => {
-    return fetch(BASE_URL + 'token-auth/', {
+    return fetch(`${BASE_URL}token-auth/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -12,7 +12,7 @@ const login = (userObject) => {
   };
   
   const getLoggedInUser = (token) => {
-    return fetch(BASE_URL + 'ispy/current_user/', {
+    return fetch(`${BASE_URL}ispy/current_user/`, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `JWT ${token}`
@@ -21,7 +21,7 @@ const login = (userObject) => {
   };
   
   const signupUser = (userObject) => {
-    return fetch(BASE_URL + 'ispy/users/', {
+    return fetch(`${BASE_URL}ispy/users/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
